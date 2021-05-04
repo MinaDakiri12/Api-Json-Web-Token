483c63b7f8ca513e68a67f1ac6add3b33ab90847
MinaDakiri <minadakiri7@gmail.com>
2021-05-04 23:12:48 +0000

    Create Tech.jsx

---
+++ Front-end/src/core/Tech.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Tech = () =>{
+    return (
+        <div>
+          <Layout title="Technicien"></Layout>
+        </div>
+    )
+}
+
+export default Tech
