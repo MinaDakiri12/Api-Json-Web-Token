ce08f4e8e36354fd007b979564f42aa4e5d8941e
MinaDakiri <minadakiri7@gmail.com>
2021-05-04 23:12:52 +0000

    Create User.jsx

---
+++ Front-end/src/core/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+const User = () =>{
+    return (
+        <div>
+          <Layout title="User"></Layout>
+        </div>
+    )
+}
+
+export default User
