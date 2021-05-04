da388717e46e73af6fe2d42f837e58702fc995de
MinaDakiri <minadakiri7@gmail.com>
2021-05-04 15:05:42 +0000

    Update auth.js

--- Back-end/Middlewares/auth.js
+++ Back-end/Middlewares/auth.js
@@ -3,21 +3,21 @@ const jwt=require('jsonwebtoken')
 require('dotenv').config();
 
 exports.auth = (Role) => (req, res, next) => {
-    const  token  = req.cookies;
-    console.log(token);
-    if (token) {
-      jwt.verify(token, process.env.JWT, async (err, decodedtoken) => {
-        console.log(decodedtoken);
-        if (!err && decodedtoken.role === Role) {
-          res.currentUser = await User.findOne({ _id: decodedtoken.id })
-          next();
-        } else {
-          return res
-            .clearCookie('token')
-            .json({ isAuth: false, role: '', ifError: 'ifError' });
-        }
-      });
-    } else {
-      return res.json({ isAuth: false, role: '', ifNotToken: 'ifNotToken' });
-    }
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedtoken) => {
+      if (!err && decodedtoken.role === Role) {
+        res.currentUser = await User.findOne({ _id: decodedtoken.id }).select(
+          '-password -enabled'
+        );
+        next();
+      } else {
+        return res
+          .clearCookie('token')
+          .json({ isAuth: false, role: '', ifError: 'ifError' });
+      }
+    });
+  } else {
+    return res.json({ isAuth: false, role: '', ifNotToken: 'ifNotToken' });
+  }
 };
