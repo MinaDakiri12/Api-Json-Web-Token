12c47a0498fe2d434d8b8792af7ffe5e1db5be18
MinaDakiri <minadakiri7@gmail.com>
2021-05-05 11:49:12 +0000

    Update route.js

--- Back-end/routes/route.js
+++ Back-end/routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const{auth} = require('../Middlewares/auth')
 const {register,login,signout,updatePassword} = require('../controllers/userController')
 
-router.post('/register', auth('admin'),register)
+router.post('/register',register)
 router.post('/login',login)
 router.post('/update/:id',updatePassword)
 router.get('/signout',signout)
